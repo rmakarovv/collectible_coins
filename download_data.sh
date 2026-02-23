a1fdd2687b480d493ffae68c440b302a314f6f69
Shinypuff <mtmnt1@mail.ru>
2026-02-23 12:31:18 +0000

    aaaa

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
-HF_TOKEN=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
+HF_TOKEN=...
 DIR=$PWD
 
 wget -P . --header="Authorization: Bearer $HF_TOKEN" https://huggingface.co/datasets/CoinGradingDL/CoinGradingDataset/resolve/main/1k-coins-dataset-no-pr.tar.gz
