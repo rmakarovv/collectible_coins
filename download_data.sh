c0c382137d08cb2b3969522234f87845a9cf1dc3
Shinypuff <mtmnt1@mail.ru>
2026-02-23 12:32:45 +0000

    jopa

--- download_data.sh
+++ download_data.sh
@@ -1,1 +1,10 @@
-ioioio
+HF_TOKEN=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
+DIR=$PWD
+
+wget -P . --header="Authorization: Bearer $HF_TOKEN" https://huggingface.co/datasets/CoinGradingDL/CoinGradingDataset/resolve/main/1k-coins-dataset-no-pr.tar.gz
+
+tar -xzvf 1k-coins-dataset-no-pr.tar.gz
+
+cd $DIR
+
+wget -P . --header="Authorization: Bearer $HF_TOKEN" https://huggingface.co/datasets/CoinGradingDL/CoinGradingDataset/resolve/main/1k-coins-dataset-no-pr.csv
